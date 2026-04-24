c609ab41c0e8c16a9d5c024fbdcbb95294f5024f
NureKukulenkoDenys <denys.kukulenko@nure.ua>
2026-04-24 20:26:04 +0300

    dz1

--- src/main/java/com/softserve/academy/model/SmsNotification.java
+++ src/main/java/com/softserve/academy/model/SmsNotification.java
@@ -1,44 +1,54 @@
 package com.softserve.academy.model;
 
-import com.softserve.academy.exception.NotDeliverableException;
-
 public class SmsNotification extends Notification {
     private String phoneNumber;
     private boolean isFlash;
 
     public SmsNotification(String recipient, String message, int priority, String phoneNumber, boolean isFlash) {
         super(recipient, message, priority);
-        // TODO: Ініціалізація додаткових полів
+        this.phoneNumber = phoneNumber;
+        this.isFlash = isFlash;
     }
 
     @Override
     public boolean isDeliverable() {
-        // TODO: Номер починається з + і має довжину 10-15 символів
-        return false;
+        return phoneNumber != null
+                && phoneNumber.startsWith("+")
+                && phoneNumber.length() >= 10
+                && phoneNumber.length() <= 15;
     }
 
     public boolean isOverLimit() {
-        // TODO: true якщо message > 160 символів
-        return false;
+        return message != null && message.length() > 160;
     }
 
     @Override
     public String getFormattedMessage() {
-        // TODO: Обрізає до 160 символів якщо довше
-        return null;
+        if (message == null) return "";
+
+        if (message.length() > 160) {
+            return message.substring(0, 160);
+        }
+
+        return message;
     }
 
     @Override
     public int estimateDeliverySeconds() {
-        // TODO: 5
-        return 0;
+        return 5;
     }
 
     @Override
     protected void performSend() {
-        // TODO: Симуляція відправки (println)
+        System.out.println("Sending SMS to " + phoneNumber);
+        System.out.println(getFormattedMessage());
     }
 
-    public String getPhoneNumber() { return phoneNumber; }
-    public boolean isFlash() { return isFlash; }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public boolean isFlash() {
+        return isFlash;
+    }
 }
